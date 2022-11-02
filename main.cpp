3b4a666d6bc9e0bec722c3c30d2cda9bbfce106c
KhaledHamza <khaledhamza@aucegypt.edu>
2022-11-03 00:35:30 +0200

    100%

--- main.cpp
+++ main.cpp
@@ -1,56 +1,45 @@
 #include"hashLL.h"
 #include"hashDyn.h"
+#include"employee.h"
 #include<iostream>
 #include<string>
 #include<fstream>
+#include<vector>
 using namespace std;
 
-void readData(string** &table, int row, int &line){
+void readData(vector<Employee> &emp, int row, int &line){
     ifstream myfile;
     myfile.open("data.txt");
     string mytext[36];//36 is the number of lines in data.txt
-    for(int i=0;i<36;i++){
-        getline(myfile,mytext[i]);
-    }
-    for(int i=0;i<4;i++){
-        table[row][i]=mytext[line];line++;
-    }
+    for(int i=0;i<36;i++) getline(myfile,mytext[i]);
+    emp[row].set_Name(mytext[line]);line++;
+    emp[row].set_age(mytext[line]);line++;
+    emp[row].set_salary(mytext[line]);line++;
+    emp[row].set_exp(mytext[line]);line++;
 }
 
 int main(){
     HashTable HLL;
     HashTabDyn HDyn;
-    HLL.insert("Mina",30,10000,4);
-    HLL.insert("Fawzy",45,5000,8);
-    HLL.insert("Yara",19,2000,0);
-    HLL.insert("Mariam",32,8000,2);
-    HLL.insert("Ayman",33,4000,8);
-    HLL.insert("Roshdy",28,9000,3);
-    HLL.insert("Aya",26,6000,3);
-    HLL.insert("Abdallah",29,7000,4);
-    HLL.insert("Fatma",21,3000,1);
-    HLL.print();
-    cout<<"--------------------------------\n";
-    HLL.remove("Mariam");
-    HLL.print();
-    cout<<"--------------------------------\n";
-
-    string** table = new string*[9];
-    for(int i=0;i<9;i++){
-        table[i]=new string[4];
-    }
+    vector<Employee> emp;
+    emp.resize(9);
     int line=0;
-    for(int i=0;i<9;i++){
-        readData(table,i,line);
+    for(int i=0;i<emp.size();i++){
+        readData(emp,i,line);
     }
 
-    for(int i=0;i<9;i++){
-        HDyn.insert(table[i]);
-    }
+    for(int i=0;i<emp.size();i++) {HLL.insert(emp[i]);}
+    HLL.print();
+    cout<<"--------------------------------\n";
+    HLL.remove(emp[3].get_Name());
+    HLL.print();
+    cout<<"--------------------------------\n";
+
+    for(int i=0;i<emp.size();i++) HDyn.insert(emp[i]);
 
     HDyn.print();
     cout<<"--------------------------------\n";
-    HDyn.remove(table[3]);
+    HDyn.remove("Mina");
     HDyn.print();
 
     return 0;
